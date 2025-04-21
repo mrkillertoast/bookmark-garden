362e0e3e5c3aa57fe9e298c497d8e7800ce7e3b7
mrkillertoast <lueku.rauscher@gmail.com>
2025-04-21 14:21:42 +0200

    feat: add Pinia for state management and update nuxt.config for module integration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,31 +3,31 @@ import tailwindcss from "@tailwindcss/vite";
 import { process } from "std-env";
 
 export default defineNuxtConfig({
-	compatibilityDate: '2024-11-01',
-	devtools: { enabled: true },
-	css: [ '~/assets/css/tailwind.css', "~/assets/css/main.css" ],
-	ssr: false, // Disable Server-Side Rendering
+				compatibilityDate: '2024-11-01',
+				devtools: { enabled: true },
+				css: [ '~/assets/css/tailwind.css', "~/assets/css/main.css" ],
+				ssr: false, // Disable Server-Side Rendering
 
-	vite: {
-		plugins: [
-			tailwindcss(),
-		],
-	},
+				vite: {
+								plugins: [
+												tailwindcss(),
+								],
+				},
 
-	modules: [ 'shadcn-nuxt', '@nuxt/icon' ],
+				modules: ['shadcn-nuxt', '@nuxt/icon', '@pinia/nuxt'],
 
-	shadcn: {
-		prefix: '',
-		componentDir: './components/ui'
-	},
+				shadcn: {
+								prefix: '',
+								componentDir: './components/ui'
+				},
 
-	runtimeConfig: {
-		public: {
-			appwriteEndpoint: process.env.NUXT_PUBLIC_APPWRITE_ENDPOINT,
-			appwriteProjectId: process.env.NUXT_PUBLIC_APPWRITE_PROJECT_ID,
-			appwriteDatabaseId: process.env.NUXT_PUBLIC_APPWRITE_DATABASE_ID,
-			appwriteBookmarksCollectionId: process.env.NUXT_PUBLIC_APPWRITE_BOOKMARKS_COLLECTION_ID,
-			appwriteClassificationsCollectionId: process.env.NUXT_PUBLIC_APPWRITE_CLASSIFICATIONS_COLLECTION_ID,
-		}
-	},
+				runtimeConfig: {
+								public: {
+												appwriteEndpoint: process.env.NUXT_PUBLIC_APPWRITE_ENDPOINT,
+												appwriteProjectId: process.env.NUXT_PUBLIC_APPWRITE_PROJECT_ID,
+												appwriteDatabaseId: process.env.NUXT_PUBLIC_APPWRITE_DATABASE_ID,
+												appwriteBookmarksCollectionId: process.env.NUXT_PUBLIC_APPWRITE_BOOKMARKS_COLLECTION_ID,
+												appwriteClassificationsCollectionId: process.env.NUXT_PUBLIC_APPWRITE_CLASSIFICATIONS_COLLECTION_ID,
+								}
+				},
 })
